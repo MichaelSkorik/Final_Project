3512dd328c00077ec9815d42bea65760a90950d1
Michael <Michael@SKorik.com>
2023-08-30 13:38:41 +0300

    Добавленно: запрос массива, автоуборка лишних пробелов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите элементы массива через запятую: ");
+string input = Console.ReadLine();
+string[] inputArray = input.Split(',');
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    inputArray[i] = inputArray[i].Trim();
+}
